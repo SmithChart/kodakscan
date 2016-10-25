SmithChart/kodakscan

commit 53158b5be6a8201ce157e59c316fdc1cad007e34

don't delete existing documents after scanimage exits with errors

scanKodak.sh
@@ -57,6 +57,10 @@ echo Will output to file "$FILE"
 #Scan pages and store .tiffs
 echo Starting scan...
 scanimage --format tiff -p --batch=$TMPFILE%04d.tiff $page_count --source "$paper_source" --resolution $dpi
+if [ ! -e $TMPFILE*.tiff ]; then
+	echo "Errors while scanning. Exiting without action."
+	exit 1
+fi
 
 #use imagemagick to compress all tiffs and glue them to a single pdf
 echo Glueing pages...

commit b82bb0af09f2d9b40d9a17b60fb8a9bee40575fb

don't error out for multipage scans
This error was introduced by myself in commit 53158b5. Sorry for that.

scanKodak.sh
@@ -58,7 +58,7 @@ echo Will output to file "$FILE"
 #Scan pages and store .tiffs
 echo Starting scan...
 scanimage --format tiff -p --batch=$TMPFILE%04d.tiff $page_count $correction --source "$paper_source" --resolution $dpi
-if [ ! -e $TMPFILE*.tiff ]; then
+if [ -z "`ls -1 $TMPFILE*.tiff`" ]; then
 	echo "Errors while scanning. Exiting without action."
 	exit 1
 fi